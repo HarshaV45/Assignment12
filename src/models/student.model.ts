20e0598ec4d805c96368e888bebc613222d80fe1
Harsha Vadlavalli <harshavadlavalli@MacBook-Pro.local>
2024-11-10 19:31:43 -0500

    Initial commit

---
+++ src/models/student.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Student extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+  })
+  class?: number;
+
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
